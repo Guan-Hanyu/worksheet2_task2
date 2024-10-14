4a9ffa2a2320701fe1a4c1bc19fcd93a534f0671
Guan-Hanyu <vbvx3311@leeds.ac.uk>
2024-10-14 21:42:01 +0000

    task

---
+++ Task2/a.py
@@ -0,0 +1,17 @@
+day_of_week = {
+    "Monday" : 1,
+    "Tuesday" : 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+    "Sunday": 7
+}
+
+day = input("Please enter a day of week: ").lower().strip().capitalize()
+
+if day in day_of_week :
+    day_number = day_of_week[day]
+    print(f"{day} is {day_number}")
+else :
+    print("Please enter a valid day")
