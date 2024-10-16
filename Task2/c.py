3d92e2fe6890709e8a7a34e8b4c8ef3741228a91
Guan-Hanyu <vbvx3311@leeds.ac.uk>
2024-10-16 14:34:49 +0000

    task

--- Task2/c.py
+++ Task2/c.py
@@ -1,12 +1,18 @@
-# In task_c.py write a program which:
-# - Asks users to enter a password which should consist of letters and numbers only. Punctuation can 
-password = input("Please enter a password: ")
-# be ignored – the tests will not check for punctuation handling.
-# - Checks that the password is: 
-# - A minimum of 8 characters long
-# - Contains more than 1 type of character (not just letters, not just numbers)
-# - If the password is valid print:
-# Your password is valid
-# - If the password is not valid print:
-# Your password must contain at least 8 characters, and a mix of letters and numbers
-# Hint: You will need to use Boolean operators and some basic string manipulation functions.
+def check_password(password):
+    if len(password) < 8:
+        return False
+    
+    contains_letter = any(char.isalpha() for char in password)
+    contains_number = any(char.isdigit() for char in password)
+
+    if contains_letter and contains_number:
+        return True
+    else:
+        return False
+
+password = input("Please enter a password : ")
+
+if check_password(password):
+    print("Your password is valid. ")
+else:
+    print("Your password must contain at least 8 characters, and a mix of letters and numbers. ")
